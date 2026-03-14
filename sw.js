159c80402b1e2aebe742c5781d1c818eec0ede73
hybridpicker <lukas@schoensgibl.com>
2026-03-14 14:30:29 +0100

    Fix SW: skip non-http requests to avoid chrome-extension cache error

--- sw.js
+++ sw.js
@@ -39,6 +39,8 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  if (!event.request.url.startsWith('http')) return;
+
   event.respondWith(
     caches.match(event.request)
       .then((cached) => {
